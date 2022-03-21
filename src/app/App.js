0dfad111a8ed673b3d32956d50cc5b4255a4880e
Yehor-F <argo.yegor@gmail.com>
2022-03-21 12:22:23 +0100

    register login 1

--- src/app/App.js
+++ src/app/App.js
@@ -7,21 +7,23 @@ import NavBar from './components/ui/navBar'
 import { ToastContainer } from 'react-toastify'
 import ProfessionProvider from './hooks/useProfession'
 import QualitiesProvider from './hooks/useQualities'
-
+import AuthProvider from './hooks/useAuth'
 function App() {
   return (
     <div>
-      <NavBar />
-      <ProfessionProvider>
-        <QualitiesProvider>
-          <Switch>
-            <Route path='/users/:userId?/:edit?' component={Users} />
-            <Route path='/login/:type?' component={Login} />
-            <Route path='/' exact component={Main} />
-            <Redirect to='/' />
-          </Switch>
-        </QualitiesProvider>
-      </ProfessionProvider>
+      <AuthProvider>
+        <NavBar />
+        <ProfessionProvider>
+          <QualitiesProvider>
+            <Switch>
+              <Route path='/users/:userId?/:edit?' component={Users} />
+              <Route path='/login/:type?' component={Login} />
+              <Route path='/' exact component={Main} />
+              <Redirect to='/' />
+            </Switch>
+          </QualitiesProvider>
+        </ProfessionProvider>
+      </AuthProvider>
       <ToastContainer />
     </div>
   )
